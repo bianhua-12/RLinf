9952c45d9b9b1a38827d1a25b6ab8da190e3c3f7
SagShang <sagshang@gmail.com>
2026-08-10 16:14:38 +0800

    修复脚踏板必须获取焦点的问题

--- run_collect_data.sh
+++ run_collect_data.sh
@@ -1,5 +1,5 @@
 ray stop --force >/dev/null 2>&1 || true; pkill -TERM -f 'single_fr3\.launch\.py|__ns:=/?(left|right)([[:space:]]|$)' 2>/dev/null || true; sleep 2; pkill -KILL -f 'single_fr3\.launch\.py|__ns:=/?(left|right)([[:space:]]|$)' 2>/dev/null || true
-export RLINF_TASK_DESCRIPTION="replay test"
+export RLINF_TASK_DESCRIPTION="fold the clothes"
 bash examples/embodiment/collect_data.sh \
     realworld_collect_data_ros2_gello_dual_franka_pnp.yaml \
-    runner.num_data_episodes=1
+    runner.num_data_episodes=10
